0f2103c42c3eda08386b39dd14c06e7edfc483ef
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-11-29 13:44:07 +0300

    add icons

--- src/components/home/ToDoItem.vue
+++ src/components/home/ToDoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="todo__container">
+  <div class="todo__container" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
     <div class="todo__main-part">
       <div
         class="todo__indicator"
@@ -10,27 +10,7 @@
       </div>
       <div class="todo__title" @click.stop="$emit('open-todo', todo)">{{ todo.title }}</div>
     </div>
-    <button class="todo__delete" @click.stop="this.showModal = true">
-      <svg
-        version="1.1"
-        id="Capa_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 490 490"
-        xml:space="preserve"
-        stroke-width="25"
-      >
-        <g id="SVGRepo_bgCarrier" stroke-width="0" />
-
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-
-        <g id="SVGRepo_iconCarrier">
-          <polygon
-            points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "
-          />
-        </g>
-      </svg>
-    </button>
+    <DeleteIcon v-if="isHovered" @click.stop="this.showModal = true" />
     <ConfirmModal
       :isVisible="showModal"
       @cancel="this.showModal = false"
@@ -42,6 +22,8 @@
 <script>
 import ConfirmModal from '@/components/base/ConfirmModal.vue'
 import DoneIcon from '@/components/icons/DoneIcon.vue'
+import DeleteIcon from '@/components/icons/DeleteIcon.vue'
+
 export default {
   props: {
     todo: {
@@ -52,11 +34,13 @@ export default {
   data() {
     return {
       showModal: false,
+      isHovered: false,
     }
   },
   components: {
     ConfirmModal,
     DoneIcon,
+    DeleteIcon,
   },
   methods: {
     handleDeleteConfirmation() {
@@ -96,31 +80,7 @@ export default {
 .todo__indicator_done {
   background-color: #fb6914;
 }
-/* .todo__indicator-svg {
-  margin: 3px 0 0 3px;
-  width: 16px;
-  height: 16px;
-  fill: #fff;
-} */
 .todo__title {
   cursor: pointer;
-}
-.todo__delete {
-  width: 16px;
-  height: 16px;
-}
-.todo__delete svg {
-  display: none;
-  width: 16px;
-  height: 16px;
-  fill: #fe2d2d;
-  stroke: #fe2d2d;
-}
-.todo__delete:hover svg {
-  fill: #d80000;
-  stroke: #d80000;
-}
-.todo__container:hover .todo__delete svg {
-  display: block;
 }
 </style>
